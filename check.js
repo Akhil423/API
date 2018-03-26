66688d3cc6b54338e4f17f6f7642055c9cef1bb5
Akhil Varma <akhilvarma423@gmail.com>
2018-03-26 22:25:41 +0530

    Update check.js

--- check.js
+++ check.js
@@ -3,7 +3,7 @@
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.url;
+      document.getElementById("demo").innerHTML = this.responseText;
     }
   };
   xhttp.open("GET", "https://akhil423.github.io/API/", true);
